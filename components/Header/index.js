9c2cf3bf912ff8a2691fece137d4c16afef4e013
GiorgiZhordania <92634370+GiorgiZhordania@users.noreply.github.com>
2022-09-05 23:00:56 +0400

    add scss

---
+++ components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from "./Style.module.scss" 
+
+
+export default function Header() {
+  return (
+    <header className={Style.header} >header component</header>
+
+  )
+}
