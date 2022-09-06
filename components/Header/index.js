e773bca3adc1744fc78494914b5fadd462e2ee22
GiorgiZhordania <92634370+GiorgiZhordania@users.noreply.github.com>
2022-09-07 02:24:07 +0400

    next

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,7 +5,7 @@ import Style from "./Style.module.scss"
 export default function Header() {
   return (
     <header className={Style.header} >
-    <nav class='navigation'>
+    <nav className={Style.nav}>
         <a>tera bank</a>
         <a>ჩემთვის</a>
         <a>ბიზნესისთვის</a>
