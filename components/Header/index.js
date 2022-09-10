0bc11b5bfae43d4c981caba942857f2a1406394d
GiorgiZhordania <92634370+GiorgiZhordania@users.noreply.github.com>
2022-09-10 19:40:46 +0400

    added header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,22 +1,31 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { MdPerson, MdSearch } from "react-icons/md";
 
 import Button from '../Button'
 
-import Style from "./Style.module.scss" 
+import Style from "./Style.module.scss"
 
 
 export default function Header() {
   return (
-    <header className={Style.header} >
-    <nav className={Style.nav}>
-        <a>tera bank</a>
+    <header className={Style.header}>
+      <Link href="/">
+        <Image src="/logo.png" alt="Logo" width={170} height={50} />
+      </Link>
+      <nav className={Style.nav}>
         <a>ჩემთვის</a>
         <a>ბიზნესისთვის</a>
+      </nav>
+      <div className={Style.actions}>
         <Button>თერა ფეი</Button>
         <Button>ინტერნეტ ბანკი</Button>
-    </nav>
+        <MdSearch color="#971D62" size={30} />
+        <MdPerson color="#971D62" size={30} />
+      </div>
     </header>
 
   )
-  
+
 }
