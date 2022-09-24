4d46076b54c93957ff450ab379f561edeca10d2d
GiorgiZhordania <92634370+GiorgiZhordania@users.noreply.github.com>
2022-09-24 22:39:17 +0400

    update

--- pages/loans.js
+++ pages/loans.js
@@ -3,24 +3,31 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import Style from '../styles/LoansStyle.module.scss'
 import Currency from '../components/Currency'
+import CustomerLoan from '../components/CustomerLoan'
 
-export default function Loans() {
+export default function Loans({ imgSrc, title }) {
     return (
-        <div className={Style.loans}>Loans
+        <div className={Style.loans}>
+            <div className={Style.promoCard}>
+                
+                <CustomerLoan
+                    imgSrc="/photo.jpg"
+                    title="აიღე სამომხმარებლო სესხი მარტივად"
+                />
+            </div>
+            <br></br>
             <div className={Style.currency}>
                 <Currency />
                 <Currency />
             </div>
         </div>
-
     )
 }
 
 export async function getStaticProps({ locale }) {
     return {
-      props: {
-        ...(await serverSideTranslations(locale, ['common'])),
-      },
+        props: {
+            ...(await serverSideTranslations(locale, ['common'])),
+        },
     };
-  }
-  
+}
