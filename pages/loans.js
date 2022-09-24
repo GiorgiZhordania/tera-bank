cb6373dfd1e3639186e81ef42388f3006455ee36
GiorgiZhordania <92634370+GiorgiZhordania@users.noreply.github.com>
2022-09-24 19:59:12 +0400

    loans

--- pages/loans.js
+++ pages/loans.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Style from '../styles/LoansStyle.module.scss'
+import Currency from '../components/Currency'
 
 export default function Loans() {
-  return (
-    <div>Loans</div>
-  )
+    return (
+        <div className={Style.loans}>Loans
+            <div className={Style.currency}>
+                <Currency />
+                <Currency />
+            </div>
+        </div>
+
+    )
 }
