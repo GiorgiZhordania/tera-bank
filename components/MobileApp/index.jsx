d58f9bacd2b4c2235f2d14540da24fed95d1e316
GiorgiZhordania <92634370+GiorgiZhordania@users.noreply.github.com>
2022-09-24 18:36:11 +0400

    application section update

--- components/MobileApp/index.jsx
+++ components/MobileApp/index.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import Button from '../Button'
 import Style from './Style.module.scss'
-import {FaGooglePlay,FaApple} from 'react-icons/fa'
+import { FaGooglePlay, FaApple } from 'react-icons/fa'
 
 
-export default function MobileApp({imgSrc,title,text}) {
-  return (
-    <div className={Style.app}>
-        <div className={Style.text}>
-            <h3>{title}</h3>
-             <br></br>
-            <p>{text}</p>
-             <br></br>
-            <div className={Style.appButton}>
-                <Button type='dark' addon={<FaGooglePlay/>}>Google Play</Button>               
-                <Button type='dark' addon={<FaApple/>}>App Store</Button>
+export default function MobileApp({ imgSrc, title, text }) {
+    return (
+        <div className={Style.app}>
+            <div className={Style.text}>
+                <h3>{title}</h3>
+                <br></br>
+                <p>{text}</p>
+                <br></br>
+                <div className={Style.appButton}>
+                    <Button type='dark' addon={<FaGooglePlay />}>Google Play</Button>
+                    <Button type='dark' addon={<FaApple />}>App Store</Button>
+                </div>
             </div>
+            <div className={Style.photo} style={{ backgroundImage: `url(${imgSrc})` }} />
         </div>
-        <div className={Style.photo}>
-            <img src={imgSrc} alt="" />
-        </div>
-    </div>
-     )
+    )
 }
